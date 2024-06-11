dd9046e59690233072e5bfaff7d0919abadc1a84
BekhanQ <bekhan.qubanov@gmail.com>
2024-06-11 15:52:13 +0600

    hero

---
+++ hero.py
@@ -0,0 +1,29 @@
+class SuperHero:
+    people = 'people'
+
+    def __init__(self, name, nickname, superpower, health_points, catchphrase):
+        self.name = name
+        self.nickname = nickname
+        self.superpower = superpower
+        self.health_points = health_points
+        self.catchphrase = catchphrase
+
+    def display_name(self):
+        print("Hero's name:", self.name)
+
+    def double_health(self):
+        self.health_points *= 2
+
+    def __str__(self):
+        return f"Nickname: {self.nickname}, Superpower: {self.superpower}, Health: {self.health_points}"
+
+    def __len__(self):
+        return len(self.catchphrase)
+
+
+# Создание объекта класса SuperHero и применение методов
+hero = SuperHero("Bruce Wayne", "Batman", "Intelligence and gadgets", 100, "I'm Batman!")
+hero.display_name()
+hero.double_health()
+print(hero)
+print("Length of catchphrase:", len(hero.catchphrase))
